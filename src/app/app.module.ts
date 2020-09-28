edc4250b96ba831d8c294faab49840e364569bc8
Paul Ngisiro <jngisiro@gmail.com>
2020-09-28 14:38:24 +0300

    :pushpin: feat: added product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,13 @@ import { CatalogueModule } from './catalogue/catalogue.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, CatalogueModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CatalogueModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/categories.ts
@@ -0,0 +1,10 @@
+const categories = {
+  "5f3eb57dd6382109acfc29d2": "Nutritionals",
+  "5f3eb5f3d6382109acfc29d3": "Aloe Drinks",
+  "5f3eb65ad6382109acfc29d4": "Weight Management",
+  "5f3eb676d6382109acfc29d5": "Bee Products",
+  "5f3eb696d6382109acfc29d6": "Personal and Skin Care",
+  "5f3eb6a8d6382109acfc29d7": "Infinite Forever",
+};
+
+export default categories;
